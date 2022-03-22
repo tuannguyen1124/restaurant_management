e36e675cd5baf3123cf6550069c147ef1d45a6bd
tuannguyen1124 <xuantuan1124@gmail.com>
2022-03-22 23:52:01 +0700

    release 03.2021

---
+++ resources/views/management/inc/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-4">
+    <div class="list-group">
+        <a href="/management/category" class="list-group-item list-group-item-action"><i
+                class="fas fa-align-justify"></i>
+            Category</a>
+        <a href="/management/menu" class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i>
+            Menu</a>
+        <a href="/management/table" class="list-group-item list-group-item-action"><i class="fas fa-chair"></i>
+            Table</a>
+        <a href="/management/user" class="list-group-item list-group-item-action"><i class="fas fa-user-friends"></i>
+            User</a>
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index');
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function(){
+   
+    
+    //routes for cashier
+    
+    Route::get('/cashier','Cashier\CashierController@index');
+    
+    Route::get('/cashier/getMenuByCategory/{category_id}','Cashier\CashierController@getMenuByCategory');
+    
+    Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
+    
+    Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+    
+    Route::post('/cashier/orderFood','Cashier\CashierController@orderFood');
+    
+    Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+    
+    Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
+    
+    Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+    
+    Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+    
+    Route::post('/cashier/increase-quantity', 'Cashier\CashierController@increaseQuantity');
+
+    Route::post('/cashier/decrease-quantity', 'Cashier\CashierController@decreaseQuantity');
+});
+
+
+Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
+        
+    //routes for management
+    
+    Route::resource('management/category','Management\CategoryController');
+    
+    Route::resource('management/menu','Management\MenuController');
+    
+    Route::resource('management/table','Management\TableController');
+    
+    Route::resource('management/user','Management\UserController');
+    
+    Route::get('/management',function(){return view('management.index');});
+   
+    //routes for report
+    Route::get('/report','Report\ReportController@index');
+    Route::get('/report/show','Report\ReportController@show');
+    
+    
+    //export to excel
+    Route::get('/report/show/export','Report\ReportController@export');
+    
+});
